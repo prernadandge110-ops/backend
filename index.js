1599b1c4c21f34cb0312c2b234ed30e9bd727cb4
prerna <prernadandge110@gmail.com>
2025-08-21 10:00:54 +0530

    message

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+mongoose.connect('mongodb://localhost:27017').then(() => console.log("connected"));
+
+const userschema = mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true }
+    }
+);
+
+const User = mongoose.model("User", userschema);
+
+userData = null;
+
+
+app.post('/register', async (req, res) => {
+    const { name, email, no, pass } = req.body;
+
+    if (!name || !email) {
+        res.status(400).send('please fill all required fields');
+    }
+
+    else {
+        res.json({
+            "Name": name,
+            "Email Address": email,
+
+
+        });
+    }
+    const newUser = new User({ name, email });
+    await newUser.save();
+
+    userData = { name, email, no, pass };
+    res.json('xyz');
+    // user: userData
+
+});
+
+
+
+app.get('/profile')
+
+app.listen(8000)
+
